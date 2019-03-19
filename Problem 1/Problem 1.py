470925e62649adf28a7aefa8fe7746ba1b46adfb
Diah Hevyka Maylawati <diah.hevyka@gmail.com>
2019-03-19 14:58:26 +0700

    Update Problem 1.py

--- Problem 1/Problem 1.py
+++ Problem 1/Problem 1.py
@@ -4,10 +4,12 @@ Created on Fri Mar 15 12:14:33 2019
 
 @author: Diah Hevyka M
 """
+n = input("Enter number that will calculate the sum of all multiples of 3 or 5 : ")
+n = int(n)
 
 jumlah = 0
 
-for i in range(1000):
+for i in range(n+1):
     if (i % 3 == 0) or (i % 5 == 0):
         jumlah = jumlah + i
 
